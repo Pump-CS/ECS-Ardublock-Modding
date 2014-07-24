8c233b34f274c31b978a8bc8debdb9d4e8f091e0
Matthew Bajzek <mattbajzek@gmail.com>
2014-07-24 12:25:17 -0500

    Added ECS LED 1-4 Blocks, Moved Generic LED to Pin

---
+++ ardublock/src/main/java/com/ardublock/translator/block/ECSLedOff4.java
@@ -0,0 +1,27 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.DigitalOutputBlock;
+import com.ardublock.translator.block.NumberBlock;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class ECSLedOff4 extends TranslatorBlock
+{
+	public ECSLedOff4(Long blockId, Translator translator, String codePrefix,	String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException , SubroutineNotDeclaredException
+	{
+		int CONST_PIN = 13;
+
+		// Add a line to the setup section that sets this pin as output
+		translator.addSetupCommand("pinMode( " + CONST_PIN + " , OUTPUT);");
+		
+		return "digitalWrite( " + CONST_PIN + " , LOW);\n";
+	}
+}
