097c0e79066f6df15676ca70c6c4552603e95d9d
Matthew Bajzek <mattbajzek@gmail.com>
2014-07-24 13:31:19 -0500

    Added "Button Pressed" Block
    Block still requires testing on Leonardo

---
+++ ardublock/src/main/java/com/ardublock/translator/block/ECSButtonPressedBlock.java
@@ -0,0 +1,31 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class ECSButtonPressedBlock extends TranslatorBlock
+{
+	public ECSButtonPressedBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+	
+		String number;
+		number = translatorBlock.toCode();
+		int pinAdjust = 1;
+		pinAdjust += Integer.parseInt(number.trim());
+		number = "" + pinAdjust;
+		translator.addInputPin(number);
+		
+		String ret = "digitalRead(";
+		ret = ret + number;
+		ret = ret + ")";
+		return codePrefix + ret + codeSuffix;
+	}
+}
