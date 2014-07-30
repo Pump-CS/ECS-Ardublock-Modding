6fd7863ba76b80366296c11ab53bf28e8e6c9384
Matthew Bajzek <mattbajzek@gmail.com>
2014-07-29 19:01:21 -0500

    Updated Pins

--- ardublock/src/main/java/com/ardublock/translator/block/NoToneBlock.java
+++ ardublock/src/main/java/com/ardublock/translator/block/NoToneBlock.java
@@ -2,6 +2,7 @@ package com.ardublock.translator.block;
 
 import com.ardublock.translator.Translator;
 import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.InvalidPinException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class NoToneBlock extends TranslatorBlock
@@ -15,6 +16,14 @@ public class NoToneBlock extends TranslatorBlock
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
 		TranslatorBlock pinBlock = this.getRequiredTranslatorBlockAtSocket(0);
+
+		if (!(pinBlock.toCode().equals(SPEAKER_PIN)) && !(pinBlock.toCode().equals(FREE_PIN_1))
+			&& !(pinBlock.toCode().equals(FREE_PIN_2)) && !(pinBlock.toCode().equals(FREE_PIN_3))
+			&& !(pinBlock.toCode().equals(FREE_PIN_4)) && !(pinBlock.toCode().equals(FREE_PIN_5))
+			&& !(pinBlock.toCode().equals(FREE_PIN_6))) {
+			throw new InvalidPinException(blockId);
+		}
+
 		String ret = "\tnoTone(" + pinBlock.toCode() + ");\n";
 		return ret;
 	}
