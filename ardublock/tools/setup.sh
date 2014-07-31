f8dc1ea07a80807c22543d116a4ee79bc1ade078
Tyler Much <tylermuch@gmail.com>
2014-07-31 11:22:03 -0500

    Update setup scripts

--- ardublock/tools/setup.sh
+++ ardublock/tools/setup.sh
@@ -2,3 +2,6 @@ cd ..
 mvn validate
 cd openblocks
 mvn install
+cd ../tools
+mvn install:install-file -Dfile=../lib/RXTXcomm.jar -DgroupId=local -DartifactId=RXTXcomm -Dversion=1.0 -Dpackaging=jar -DlocalRepositoryPath=../repo -DcreateChecksum=true
+cat maven-pom-additions
