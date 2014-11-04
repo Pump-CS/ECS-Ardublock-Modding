8d681e7bddf31fad5ba4c528301bdd2b91a07d7c
Tyler Much <tylermuch@gmail.com>
2014-11-04 17:31:05 -0600

    Add flat notes and remove notes hashtable duplicate.
    notemap (hashtable maping note names to frequencies) is now a public static final field in ECSPlayNoteBlock

--- ardublock/src/main/java/com/ardublock/translator/block/ecs/ECSPlayNoteBlock.java
+++ ardublock/src/main/java/com/ardublock/translator/block/ecs/ECSPlayNoteBlock.java
@@ -11,119 +11,162 @@ import java.util.Hashtable;
 public class ECSPlayNoteBlock extends TranslatorBlock
 {
 
-	private Hashtable<String, Integer> notes = new Hashtable<String, Integer>();
+	public static final Hashtable<String, Integer> notemap = new Hashtable<String, Integer>() {{
+		put("C1",   33);
+		put("C1#",  35);
+		put("D1b",  35);
+		put("D1",   37);
+		put("D1#",  39);
+		put("E1b",	39);
+		put("E1",   41);
+		put("F1",   44);
+		put("F1#",  46);
+		put("G1b",	46);
+		put("G1",   49);
+		put("G1#",  52);
+		put("A1b",	52);
+		put("A1",   55);
+		put("A1#",  58);
+		put("B1b",	58);
+		put("B1",   62);
+
+		put("C2",   65);
+		put("C2#",  69);
+		put("D2b",	69);
+		put("D2",   73);
+		put("D2#",  78);
+		put("E2b",	78);
+		put("E2",   82);
+		put("F2",   87);
+		put("F2#",  93);
+		put("G2b",	93);
+		put("G2",   98);
+		put("G2#", 104);
+		put("G2b", 104);
+		put("A2",  110);
+		put("A2#", 117);
+		put("B2b", 117);
+		put("B2",  123);
+
+		put("C3",  131);
+		put("C3#", 139);
+		put("D3b", 139);
+		put("D3",  147);
+		put("D3#", 156);
+		put("E3b", 156);
+		put("E3",  165);
+		put("F3",  175);
+		put("F3#", 185);
+		put("G3b", 185);
+		put("G3",  196);
+		put("G3#", 208);
+		put("A3b", 208);
+		put("A3",  220);
+		put("A3#", 233);
+		put("B3b", 233);
+		put("B3",  247);
+
+		put("C",   262);
+		put("C#",  277);
+		put("Db",  277);
+		put("D",   294);
+		put("D#",  311);
+		put("Eb",  311);
+		put("E",   330);
+		put("F",   349);
+		put("F#",  370);
+		put("Gb",  370);
+		put("G",   392);
+		put("G#",  415);
+		put("Ab",  415);
+		put("A",   440);
+		put("A#",  466);
+		put("Bb",  466);
+		put("B",   494);
+		put("C4",  262);
+		put("C4#", 277);
+		put("D4b", 277);
+		put("D4",  294);
+		put("D4#", 311);
+		put("E4b", 311);
+		put("E4",  330);
+		put("F4",  349);
+		put("F4#", 370);
+		put("G4b", 370);
+		put("G4",  392);
+		put("G4#", 415);
+		put("A4b", 415);
+		put("A4",  440);
+		put("A4#", 466);
+		put("B4b", 466);
+		put("B4",  494);
+
+		put("C5",  523);
+		put("C5#", 554);
+		put("D5b", 554);
+		put("D5",  587);
+		put("D5#", 622);
+		put("E5b", 622);
+		put("E5",  659);
+		put("F5",  698);
+		put("F5#", 740);
+		put("G5b", 740);
+		put("G5",  784);
+		put("G5#", 831);
+		put("A5b", 831);
+		put("A5",  880);
+		put("A5#", 932);
+		put("B5b", 932);
+		put("B5",  988);
+
+		put("C6",  1047);
+		put("C6#", 1109);
+		put("D6b", 1109);
+		put("D6",  1175);
+		put("D6#", 1245);
+		put("E6b", 1245);
+		put("E6",  1319);
+		put("F6",  1397);
+		put("F6#", 1480);
+		put("G6b", 1480);
+		put("G6",  1568);
+		put("G6#", 1661);
+		put("A6b", 1661);
+		put("A6",  1760);
+		put("A6#", 1865);
+		put("B6b", 1865);
+		put("B6",  1976);
+
+		put("C7",  2093);
+		put("C7#", 2217);
+		put("D7b", 2217);
+		put("D7",  2349);
+		put("D7#", 2489);
+		put("E7b", 2489);
+		put("E7",  2637);
+		put("F7",  2794);
+		put("F7#", 2960);
+		put("G7b", 2960);
+		put("G7",  3136);
+		put("G7#", 3322);
+		put("A7b", 3322);
+		put("A7",  3520);
+		put("A7#", 3729);
+		put("B7b", 3729);
+		put("B7",  3951);
+
+		put("C8",  4186);
+		put("C8#", 4435);
+		put("D8b", 4435);
+		put("D8",  4699);
+		put("D8#", 4978);
+		put("E8b", 4978);
+
+	}};
 
 	public ECSPlayNoteBlock(Long blockId, Translator translator, String codePrefix,	String codeSuffix, String label)
 	{
 		super(blockId, translator, codePrefix, codeSuffix, label);
-
-		notes.put("C1",   33);
-		notes.put("C1#",  35);
-		notes.put("D1",   37);
-		notes.put("D1#",  39);
-		notes.put("E1",   41);
-		notes.put("F1",   44);
-		notes.put("F1#",  46);
-		notes.put("G1",   49);
-		notes.put("G1#",  52);
-		notes.put("A1",   55);
-		notes.put("A1#",  58);
-		notes.put("B1",   62);
-
-		notes.put("C2",   65);
-		notes.put("C2#",  69);
-		notes.put("D2",   73);
-		notes.put("D2#",  78);
-		notes.put("E2",   82);
-		notes.put("F2",   87);
-		notes.put("F2#",  93);
-		notes.put("G2",   98);
-		notes.put("G2#", 104);
-		notes.put("A2",  110);
-		notes.put("A2#", 117);
-		notes.put("B2",  123);
-
-		notes.put("C3",  131);
-		notes.put("C3#", 139);
-		notes.put("D3",  147);
-		notes.put("D3#", 156);
-		notes.put("E3",  165);
-		notes.put("F3",  175);
-		notes.put("F3#", 185);
-		notes.put("G3",  196);
-		notes.put("G3#", 208);
-		notes.put("A3",  220);
-		notes.put("A3#", 233);
-		notes.put("B3",  247);
-
-		notes.put("C",   262);
-		notes.put("C#",  277);
-		notes.put("D",   294);
-		notes.put("D#",  311);
-		notes.put("E",   330);
-		notes.put("F",   349);
-		notes.put("F#",  370);
-		notes.put("G",   392);
-		notes.put("G#",  415);
-		notes.put("A",   440);
-		notes.put("A#",  466);
-		notes.put("B",   494);
-		notes.put("C4",  262);
-		notes.put("C4#", 277);
-		notes.put("D4",  294);
-		notes.put("D4#", 311);
-		notes.put("E4",  330);
-		notes.put("F4",  349);
-		notes.put("F4#", 370);
-		notes.put("G4",  392);
-		notes.put("G4#", 415);
-		notes.put("A4",  440);
-		notes.put("A4#", 466);
-		notes.put("B4",  494);
-
-		notes.put("C5",  523);
-		notes.put("C5#", 554);
-		notes.put("D5",  587);
-		notes.put("D5#", 622);
-		notes.put("E5",  659);
-		notes.put("F5",  698);
-		notes.put("F5#", 740);
-		notes.put("G5",  784);
-		notes.put("G5#", 831);
-		notes.put("A5",  880);
-		notes.put("A5#", 932);
-		notes.put("B5",  988);
-
-		notes.put("C6",  1047);
-		notes.put("C6#", 1109);
-		notes.put("D6",  1175);
-		notes.put("D6#", 1245);
-		notes.put("E6",  1319);
-		notes.put("F6",  1397);
-		notes.put("F6#", 1480);
-		notes.put("G6",  1568);
-		notes.put("G6#", 1661);
-		notes.put("A6",  1760);
-		notes.put("A6#", 1865);
-		notes.put("B6",  1976);
-
-		notes.put("C7",  2093);
-		notes.put("C7#", 2217);
-		notes.put("D7",  2349);
-		notes.put("D7#", 2489);
-		notes.put("E7",  2637);
-		notes.put("F7",  2794);
-		notes.put("F7#", 2960);
-		notes.put("G7",  3136);
-		notes.put("G7#", 3322);
-		notes.put("A7",  3520);
-		notes.put("A7#", 3729);
-		notes.put("B7",  3951);
-
-		notes.put("C8",  4186);
-		notes.put("C8#", 4435);
-		notes.put("D8",  4699);
-		notes.put("D8#", 4978);
 	}
 
 	@Override
@@ -134,7 +177,7 @@ public class ECSPlayNoteBlock extends TranslatorBlock
 		String note = freqBlock.toCode();
 		System.out.println("." + note + ".");
 		note = note.substring(1, note.length() - 1);
-		Integer freq = notes.get(note);
+		Integer freq = notemap.get(note);
 
 		if (freq == null) {
 			throw new InvalidNoteException(blockId);
