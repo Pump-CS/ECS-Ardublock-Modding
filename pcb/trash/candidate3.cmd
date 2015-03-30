ac78aa604398d6a8661bc7adf3faef08dc8a384a
Luke Mivshek <luke.mivshek@marquette.edu>
2015-03-30 13:24:53 -0500

    Replaced pcb directory with updated versions.
    All pcb iterations with updated naming convention added.
    Trash directory added with extra copies of all old files in case they may be needed.

---
+++ pcb/trash/candidate3.cmd
@@ -0,0 +1,190 @@
+# Pin name action command file
+
+# Start of element S1
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 4, 4)
+
+# Start of element S2
+ChangePinName(S2, 3, 3)
+ChangePinName(S2, 1, 1)
+ChangePinName(S2, 2, 2)
+ChangePinName(S2, 4, 4)
+
+# Start of element S3
+ChangePinName(S3, 3, 3)
+ChangePinName(S3, 1, 1)
+ChangePinName(S3, 2, 2)
+ChangePinName(S3, 4, 4)
+
+# Start of element S4
+ChangePinName(S4, 3, 3)
+ChangePinName(S4, 1, 1)
+ChangePinName(S4, 2, 2)
+ChangePinName(S4, 4, 4)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element X2
+ChangePinName(X2, 2, x2)
+ChangePinName(X2, 1, x1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 10, 10)
+ChangePinName(CONN3, 9, 9)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+
+# Start of element CONN4
+ChangePinName(CONN4, 8, 8)
+ChangePinName(CONN4, 7, 7)
+ChangePinName(CONN4, 6, 6)
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 2, 2)
+ChangePinName(CONN4, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element X1
+ChangePinName(X1, 2, x2)
+ChangePinName(X1, 1, x1)
+
+# Start of element SPK1
+ChangePinName(SPK1, 2, 2)
+ChangePinName(SPK1, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element T1
+ChangePinName(T1, 2, 2)
+ChangePinName(T1, 1, 1)
+ChangePinName(T1, 3, 3)
+
+# Start of element U101
+ChangePinName(U101, 4, V-)
+ChangePinName(U101, 8, V+)
+ChangePinName(U101, 1, OUT)
+ChangePinName(U101, 2, IN-)
+ChangePinName(U101, 3, IN+)
+ChangePinName(U101, 4, V-)
+ChangePinName(U101, 8, V+)
+ChangePinName(U101, 7, OUT)
+ChangePinName(U101, 6, IN-)
+ChangePinName(U101, 5, IN+)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element LED4
+ChangePinName(LED4, 2, K)
+ChangePinName(LED4, 1, A)
+
+# Start of element LED1
+ChangePinName(LED1, 2, K)
+ChangePinName(LED1, 1, A)
+
+# Start of element LED2
+ChangePinName(LED2, 2, K)
+ChangePinName(LED2, 1, A)
+
+# Start of element LED3
+ChangePinName(LED3, 2, K)
+ChangePinName(LED3, 1, A)
